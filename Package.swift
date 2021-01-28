da8f55cbc6c611ca8ce8e0b4b6123ccf5a7cf893
Mike Quinn <mike.quinn2@workday.com>
2021-01-28 15:53:57 -0700

    Updates for Swift Package

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "QuizTrain",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "QuizTrain", targets: ["QuizTrain"])
+    ],
+    targets: [
+        .target(name: "QuizTrain", dependencies: []),
+    ],
+    swiftLanguageVersions: [.v5]
+)
